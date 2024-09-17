46c85d6d6db4a639a5a08eb813e615c92ae86df4
Diego Prado <diegoprd@gmail.com>
2024-09-17 10:52:01 +0200

    removing all the pages

--- app/constants/Form.js
+++
@@ -1,8 +0,0 @@
-export const divClass = "px-10 col-span-3";
-export const divFullClass = "px-10 col-span-full";
-export const sectionClass = "bg-[#21211E] drop-shadow-lg pb-6 rounded grid grid-cols-6 gap-y-4 mb-2";
-export const headingClass = "text-beige col-span-full pt-4 px-10 block text-2xl font-titulo mb-2 uppercase";
-export const labelClass = "block text-sm font-medium mb-2 dark:text-white";
-export const inputClass = "py-3 px-4 block text-black  w-full border rounded text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-beige dark:text-gray-400 dark:focus:ring-gray-600";
-export const buttonClass = "ml-10 mt-10 bg-transparent border border-green-500  hover:bg-green-700 text-white hover:text-black font-bold py-2 px-6 rounded"
-export const ejercicioClass = "border-t-2 border-beige-oscuro grid grid-cols-6 gap-y-4 mb-6 pr-0"
--- app/login/page.js
+++ app/login/page.js
@@ -3,7 +3,6 @@
 import '/styles/globals.css';
 import { useState } from 'react';
 import Logo from '/components/Logo';
-import { login, signup } from './actions';
 
 export default function Login() {
   const [isSigningUp, setIsSigningUp] = useState(false);
@@ -44,37 +43,7 @@ export default function Login() {
       {/* Content */}
       <main className="relative z-10 min-w-16 max-w-lg">
         {/* Form */}
-        <form className='bg-fondo pb-10 grid opacity-95 rounded' method="POST" onSubmit={handleSubmit} action={isSigningUp ? 'signup' : 'login' }>
-          <div className='justify-center mx-0 pt-6 items-center mx-auto w-full pb-5 border-white  bg-black flex border-b-2'>
-            <Logo className='justify-center'/>
-          </div>
-          <div className='pt-4 px-20 grid m-2 text-3xl font-medium font-titulo'>
-            <p>{
-              isSigningUp ? (
-                'REGISTRARSE'
-              ) : (
-                'INICIAR SESIÓN'
-              )
-            } </p>
-          </div>
-          <div className='pt-4 px-20 grid m-2 gap-2'>
-            <label htmlFor="email" className='font-bold text-left'>Email</label>
-            <input id="email" name="email" type="email" className='border p-1 rounded text-black px-2' placeholder='Ingrese su email' required />
-          </div>
-          <div className='pt-4 px-20 grid m-2 gap-2'>
-            <label htmlFor="password" className='pb-2 font-bold text-left'>Contraseña</label>
-            <input id="password" name="password" type="password" className='border p-1 rounded text-black px-2' placeholder='Ingrese su contraseña' required />
-          </div>
-          <div className="text-red-500 mx-20 w-80 flex items-center justify-center ">{errorMessage}</div>
-          <div className="text-green-500 mx-20 w-80 flex items-center justify-center ">
-          { 
-            clickedSignUp && <div>Se ha enviado una confirmación a su correo electrónico</div>
-          }
-          </div>
-          <div className='pt-2 px-20 w-80 w-full'>
-            <button type="submit" formAction={login} className="bg-beige uppercase inline hover:bg-beige-oscuro text-white font-bold py-4 px-8 rounded my-4">Iniciar sesión</button>
-          </div>
-        </form>
+       
       </main>
     </div>
   )
